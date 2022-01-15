b894fa6aa06d4a02ad6c2dbdba728a08f4eb24cc
Maxime Ryheul <maxime.ryheul@student.howest.be>
2022-01-15 18:15:00 +0100

    gedaan tiebrake

--- backend/app.py
+++ backend/app.py
@@ -7,19 +7,26 @@ import threading
 from six.moves import input
 import json
 
-rood = 0
-blauw = 0
+rood = "0"
+blauw = "0"
+tiebrake_rood = 0
+tiebrake_blauw = 0
 set_rood = 0
 set_blauw = 0
+game_rood = 0
+game_blauw = 0
+naam_blauw = "Team Blauw"
+naam_rood = "Team Rood"
+
 game1_rood = 0
 game2_rood = 0
 game3_rood = 0
 game1_blauw = 0
 game2_blauw = 0
 game3_blauw = 0
-naam_blauw = "Team Blauw"
-naam_rood = "Team Rood"
 
+total_sets = 0
+tiebrake = False
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Hier mag je om het even wat schrijven'
@@ -36,32 +43,123 @@ def connect():
     print("A new client connects")
     # run()
 
-def menu(keuze):
-    global rood,blauw,set_blauw,set_rood
-    if keuze == 1:
-        rood += 15
-        socketio.emit("B2F_verandering_punten", {'red':  rood, "blue": blauw })
-        print("item sent")
-    if keuze == 2:
-        if blauw == 45:
+def check_set():
+    global set_blauw,set_rood
+    if set_rood == 2:
+        print("rood wint")
+    if set_blauw == 2:
+        print("blauw wint")
+    socketio.emit("B2F_verandering_set", {'red':  set_rood, "blue": set_blauw })
 
-        blauw += 15
-        socketio.emit("B2F_verandering_punten", {'red':  rood, "blue": blauw })
-        print("item sent")
+def check_game():
+    global game_rood,game_blauw,set_rood,set_blauw,tiebrake
+    if game_blauw == 6 and game_rood == 6:
+        tiebrake = True
+        print("tiebrake")
+    elif game_blauw == 6:
+        if game_rood < game_blauw-1:
+            game_rood = 0
+            game_blauw = 0
+            set_blauw += 1
+            check_set()
+    elif game_rood == 6:
+        if game_blauw < game_rood-1:
+            game_rood = 0
+            game_blauw = 0
+            set_rood += 1
+            check_set()
+    socketio.emit("B2F_verandering_game", {'red':  game_rood, "blue": game_blauw })
+
+def menu(keuze):
+    global rood,blauw,set_blauw,set_rood,game_rood,game_blauw,tiebrake_blauw,tiebrake_rood,tiebrake
+    if tiebrake:
+        print("tiebrake")
+        if keuze == 1:
+            tiebrake_rood += 1
+            if tiebrake_rood >= 7 and tiebrake_blauw < tiebrake_rood-1:
+                tiebrake_rood=0
+                tiebrake_blauw=0
+                game_blauw = 0
+                game_rood = 0
+                set_rood += 1
+                check_set()
+                tiebrake = False
+        elif keuze == 2:
+            tiebrake_blauw += 1
+            if tiebrake_blauw >= 7 and tiebrake_rood < tiebrake_blauw-1:
+                tiebrake_rood=0
+                tiebrake_blauw=0
+                game_blauw = 0
+                game_rood = 0
+                set_blauw += 1
+                check_set()
+                tiebrake= False
+        socketio.emit("B2F_verandering_punten", {'red':  tiebrake_rood, "blue": tiebrake_blauw })
+    else:
+        if keuze == 1:
+            # rood += 15
+            if rood == "adv":
+                rood = "0"
+                blauw = "0"
+                game_rood += 1
+                check_game()
+            elif blauw == "40" and rood == "40":
+                # rood = "advantage"
+                rood = "adv"
+            elif rood == "40" and blauw == "adv":
+                blauw = "40"
+            elif rood == "40":
+                rood = "0"
+                blauw = "0"
+                game_rood += 1
+                check_game()
+            elif rood == "30":
+                rood = "40"
+            elif rood == "15":
+                rood = "30"
+            else:
+                rood = "15"
+            socketio.emit("B2F_verandering_punten", {'red':  rood, "blue": blauw })
+            print("item sent")
+        elif keuze == 2:
+            if blauw == "adv":
+                rood = "0"
+                blauw = "0"
+                game_blauw += 1
+                check_game()
+            elif blauw == "40" and rood == "40":
+                blauw = "adv"
+                # blauw = "advantage"
+            elif blauw == "40" and rood == "adv":
+                rood = "40"
+            elif blauw == "40":
+                rood = "0"
+                blauw = "0"
+                game_blauw += 1
+                check_game()
+            elif blauw == "30":
+                blauw = "40"
+            elif blauw == "15":
+                blauw = "30"
+            else:
+                blauw = "15"
+            # blauw += 15
+
+            socketio.emit("B2F_verandering_punten", {'red':  rood, "blue": blauw })
+            print("item sent")
     if keuze == 9:
         exit() 
- 
 
-def run():    
-    keuze = 0    
-    while keuze  != 9:                
+def run():
+    keuze = 0
+    while keuze  != 9:       
         print("Maak uw keuze:")        
         print("1. punt rood")        
         print("2. punt blauw")                
         print("9. Exit")        
         try:
             keuze = int(input())
-            print(keuze)
+            # print(keuze)
             menu(keuze)
         except Exception as ex:
             print(ex)
--- script/app.js
+++ script/app.js
@@ -19,10 +19,42 @@ const listenToSocket = function(){
         // console.log(`verbonden met socket webserver`);
         rood = document.querySelector(".js-point-red");
         blauw = document.querySelector(".js-point-blue");
+        
+        opslag_blauw = document.querySelector(".js-opslag-blue");
+        opslag_rood = document.querySelector(".js-opslag-red");
+
         console.log("item binnen gekregen")
         console.log(jsonObject);
         rood.innerHTML = jsonObject.red;
         blauw.innerHTML = jsonObject.blue;
+
+        if(opslag_blauw.innerHTML == "R"){
+            opslag_blauw.innerHTML = "";
+            opslag_rood.innerHTML = "R";
+        }else{
+            opslag_blauw.innerHTML = "R";
+            opslag_rood.innerHTML = "";
+        }
+    });
+    
+    socket.on('B2F_verandering_game', function(jsonObject){
+        // console.log(`verbonden met socket webserver`);
+        game_rood = document.querySelector(".js-game-red");
+        game_blauw = document.querySelector(".js-game-blue");
+        console.log("item binnen gekregen")
+        console.log(jsonObject);
+        game_rood.innerHTML = jsonObject.red;
+        game_blauw.innerHTML = jsonObject.blue;
+    });
+    
+    socket.on('B2F_verandering_set', function(jsonObject){
+        // console.log(`verbonden met socket webserver`);
+        set_rood = document.querySelector(".js-set-red");
+        set_blauw = document.querySelector(".js-set-blue");
+        console.log("item binnen gekregen")
+        console.log(jsonObject);
+        set_rood.innerHTML = jsonObject.red;
+        set_blauw.innerHTML = jsonObject.blue;
     });
 
     
