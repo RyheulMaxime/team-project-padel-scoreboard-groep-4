7f2aa00208e8b338a3a07817b58a71e094f9dcaf
Baptiste Van der Eecken <baptiste.van.der.eecken@student.howest.be>
2022-01-27 12:22:13 +0100

    api call clubs

---
+++ PadelScoreboard/Models/SponsorInfo.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadelScoreboard.Models
+{
+    public class SponsorInfo
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("naam")]
+        public string Naam { get; set; }
+
+        [JsonProperty("foto")]
+        public string Foto { get; set; }
+
+        [JsonProperty("extra")]
+        public string Extra { get; set; }
+    }
+}
